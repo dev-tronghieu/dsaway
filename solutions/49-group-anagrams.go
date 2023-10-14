22d1a4db3d2ac38c6b9ba071649b39c0fd71c8bd
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-14 19:42:39 +0700

    49

---
+++ solutions/49-group-anagrams.go
@@ -0,0 +1,19 @@
+package solutions
+
+func GroupAnagrams(strs []string) [][]string {
+	groups := make(map[[26]int][]string)
+	for _, str := range strs {
+		var key [26]int
+		for _, c := range str {
+			key[c-'a']++
+		}
+		groups[key] = append(groups[key], str)
+	}
+
+	result := make([][]string, 0, len(groups))
+	for _, group := range groups {
+		result = append(result, group)
+	}
+
+	return result
+}
